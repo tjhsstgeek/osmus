4076908c2ad49db2af233024317be2868e5dd6b0
tjhsstgeek <tjhsstgeek@gmail.com>
2012-02-18 18:11:02 -0500

    Started writing a new server. It currently creates a randomized level and simulates it.

---
+++ server/server2.js
@@ -0,0 +1,33 @@
+var io = require('socket.io').listen(5050);
+var __engine = new require('../common/engine.js');
+var engine = __engine.engine;
+
+var __blob = new require('../common/blob.js');
+var blob = __blob.blob;
+
+var __player = new require('../common/player.js');
+var player = __player.player;
+
+var __attractor = new require('../common/attractor.js');
+var attractor = __attractor.attractor;
+
+var level = new require('./level.js');
+
+var w = 640;
+var h = 480;
+
+var game = new engine(w, h);
+
+for (var a = 0;a < 100;a++) {
+	var b = new blob(game, Math.random() * 640, Math.random() * 480,
+	                 Math.random() * 10 - 5, Math.random() * 10 - 5,
+	                 Math.random() * 10, 1);
+	
+	game.attach(b);
+}
+
+game.timer(10);
+
+io.sockets.on('connection', function(socket) {
+	
+});
