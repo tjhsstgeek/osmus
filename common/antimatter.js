2a4084e9e4711c7e490b5da869a0e8cf097a9900
tjhsstgeek <tjhsstgeek@gmail.com>
2012-02-21 00:52:14 -0500

    Fixed up and wrote more antimatter code. Antimatter should now have a more realistic absorption. Fixed several bugs.

--- common/antimatter.js
+++ common/antimatter.js
@@ -2,16 +2,16 @@
 /**
  * This file contains code required by the client and server for blobs.
  */
-var antimatter = function(e, x, y, vx, vy, r, m) {
-	blob.call(this, e, x, y, vx, vy, r, m);
+var antimatter = function(e, x, y, vx, vy, r, density) {
+	blob.call(this, e, x, y, vx, vy, r, density);
 	this.type = e.ANTIMATTER;
 }
 /**
  * Given a json format, construct a blob
  */
 antimatter.prototype.create_json = function(params) {
-	return new antimatter(params.e, params.x, params.y, params.vx, params.vy,
-	                params.r, params.m);
+	return new antimatter(params.e, params.x, params.y, params.vx,
+	                      params.vy, params.r, params.density);
 }
 /**
  * A positive area means area is added, otherwise removed.
@@ -28,7 +28,21 @@ antimatter.prototype.compare = function(other) {
 	}
 	return 1;
 }
-
+/**
+ *
+ */
+antimatter.prototype.absorb = function(o) {
+	if (o.type == this.e.ANTIMATTER) {
+		blob.prototype.antimatter.call(this, o);
+		return;
+	}
+	var overlap = this.e.overlap(this, o);
+	//
+	var dR = overlap * (overlap - 2 * o.r) / (2 * (overlap - this.r - o.r));
+	var dr = overlap - dR;
+	this.transfer_radius(-dR);
+	o.transfer_radius(-dr);
+}
 exports.antimatter = antimatter;
 
 })(typeof global === "undefined" ? window : exports);
